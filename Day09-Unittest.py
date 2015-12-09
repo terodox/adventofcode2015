a4605d66c621ec7906cec95b8b5f6e3edd282ef7
Andrew Desmarais <adesmarais@newforma.com>
2015-12-09 13:06:48 -0500

    commit - in progress

--- Day09-Unittest.py
+++ Day09-Unittest.py
@@ -3,17 +3,57 @@ from unittest import TestSuite, makeSuite, TextTestRunner, TestCase
 
 
 class Day09UnitTests(TestCase):
-    _sut = AdventDayNine(9)
     sample_paths = [
         'London to Dublin = 464',
         'London to Belfast = 518',
         'Dublin to Belfast = 141'
     ]
 
+    def setUp(self):
+        self._sut = AdventDayNine(9)
+        self._sut.cities = {}
+
     def test_Paths_are_parsed_properly(self):
-        result = self._sut.parse_one_path(self.sample_paths[0])
-        result.city = 'London'
-        result.desintations = {'Dublin': 464}
+        self._sut.parse_one_path(self.sample_paths[0])
+        self.assertIn('London', self._sut.cities)
+        self.assertIn('Dublin', self._sut.cities)
+        self.assertIn('Dublin', self._sut.cities['London'])
+        self.assertIn('London', self._sut.cities['Dublin'])
+        self.assertEquals(464, self._sut.cities['London']['Dublin'])
+        self.assertEquals(464, self._sut.cities['Dublin']['London'])
+
+    def test_Cities_are_added_properly(self):
+        self.assertNotIn('London', self._sut.cities)
+
+        self._sut.add_update_city('London', 'Dublin', 500)
+
+        self.assertIn('London', self._sut.cities)
+        self.assertIn('Dublin', self._sut.cities['London'])
+        self.assertEquals(500, self._sut.cities['London']['Dublin'])
+
+        self._sut.add_update_city('London', 'York', 1000)
+
+        self.assertIn('London', self._sut.cities)
+        self.assertIn('Dublin', self._sut.cities['London'])
+        self.assertEquals(500, self._sut.cities['London']['Dublin'])
+        self.assertIn('London', self._sut.cities)
+        self.assertIn('York', self._sut.cities['London'])
+        self.assertEquals(1000, self._sut.cities['London']['York'])
+
+    def test_List_is_parsed_properly(self):
+        self._sut.read_data = lambda: self.sample_paths
+
+        self._sut.parse_all_paths()
+
+        self.assertEquals(len(self.sample_paths), len(self._sut.cities.items()))
+
+    def test_Gets_correct_anser_count(self):
+        self._sut.read_data = lambda: self.sample_paths
+        self._sut.parse_all_paths()
+
+        result = self._sut.get_all_possible_distances()
+
+        self.assertEquals(6, len(result))
 
 
 testSuite = TestSuite()
--- Day09.py
+++ Day09.py
@@ -3,17 +3,51 @@ import re
 
 
 class AdventDayNine(AdventDay):
+    cities = {}
+
     def answer_part1(self):
-        print("")
+        self.parse_all_paths()
+        all_distances = self.get_all_possible_distances()
+
+    def get_all_possible_distances(self):
+        final_paths = {}
+        for one_city, child_cities in dict(self.cities).items():
+            final = self.build_all_possible_paths(one_city, child_cities, [], 0)
+            final_paths
+
+        return final_paths
+
+    def build_all_possible_paths(self, current_city_name, child_cities, previous_cities, distance):
+        previous = list(previous_cities)
+        previous.append(current_city_name)
+
+        final_paths = {}
+
+        for one_city, one_city_distance in dict(child_cities).items():
+            # Add check for if city is already in previous_cities
+            if one_city in previous_cities:
+                final_paths[''.join(previous_cities)] = distance
+
+            new_distance = distance + one_city_distance
+            final_path, final_distance = self.build_all_possible_paths(one_city, self.cities[one_city], previous, new_distance)
+            final_paths[final_path] = final_distance
+
+        return final_paths
+
+    def parse_all_paths(self):
+        for one_path in self.read_data():
+            self.parse_one_path(one_path)
 
     def parse_one_path(self, one_path):
-        path_parts = re.findall(r'([^\s]+)\s([^\s]+)\s=\s([^\s]+)', one_path)
+        path_parts = re.findall(r'([^\s]+)\sto\s([^\s]+)\s=\s([^\s]+)', one_path)[0]
+        city1 = path_parts[0]
+        city2 = path_parts[1]
+        distance = path_parts[2]
+        self.add_update_city(city1, city2, int(distance))
+        self.add_update_city(city2, city1, int(distance))
 
-
-class City:
-    def __init__(self, city):
-        self.city = city
-        self.desintations = {}
-
-    def add_destination(self, city, distance):
-        self.desintations[city] = distance
+    def add_update_city(self, city_name1, city_name2, distance):
+        if city_name1 not in self.cities:
+            self.cities[city_name1] = {city_name2: distance}
+        elif city_name2 not in self.cities[city_name1]:
+            self.cities[city_name1][city_name2] = distance
