f80a128beaa4589f6c02c063556f0e6e0ff9179c
Andy Desmarais <adesmarais@newforma.local>
2015-12-18 06:44:22 -0500

    Day 18 started

---
+++ Day18-Unittest.py
@@ -0,0 +1,79 @@
+from Day18 import *
+from unittest import TestSuite, makeSuite, TextTestRunner, TestCase
+
+
+class Day18UnitTests(TestCase):
+    def setUp(self):
+        self._sut = AdventDayEighteen()
+        self.test_input = [
+            '.#.#.#',
+            '...##.',
+            '#....#',
+            '..#...',
+            '#.#..#',
+            '####..'
+        ]
+        self._sut.read_data = lambda: self.test_input
+
+    def test_count_lights(self):
+        test_input = [[1, 2], [3, 4]]
+        self.assertEqual(10, self._sut.count_lights(test_input))
+
+    def test_get_initial_lights(self):
+        lights = self._sut.get_initial_lights()
+
+        result = self._sut.count_lights(lights)
+
+        self.assertEqual(15, result)
+
+    def test_get_neighbors_value(self):
+        lights_in = self._sut.get_initial_lights()
+
+        self.assertEqual(0, self._sut.get_neighbors_value(lights_in, 0, 0))
+
+    def test_iterate_lights(self):
+        lights_in = self._sut.get_initial_lights()
+
+        lights = self._sut.iterate_lights(lights_in)
+
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 0, 0))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 0, 1))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 0, 2))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 0, 3))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 0, 4))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 0, 5))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 1, 0))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 1, 1))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 1, 2))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 1, 3))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 1, 4))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 1, 5))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 2, 0))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 2, 1))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 2, 2))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 2, 3))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 2, 4))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 2, 5))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 3, 0))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 3, 1))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 3, 2))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 3, 3))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 3, 4))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 3, 5))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 4, 0))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 4, 1))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 4, 2))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 4, 3))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 4, 4))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 4, 5))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 5, 0))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 5, 1))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 5, 2))
+        self.assertEqual(1, self._sut.get_neighbors_value(lights, 5, 3))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 5, 4))
+        self.assertEqual(0, self._sut.get_neighbors_value(lights, 5, 5))
+
+testSuite = TestSuite()
+testSuite.addTest(makeSuite(Day18UnitTests))
+runner = TextTestRunner(verbosity=2)
+runner.run(testSuite)
---
+++ Day18.py
@@ -0,0 +1,57 @@
+from DataReader import AdventDay
+import itertools
+
+
+class AdventDayEighteen(AdventDay):
+    def __init__(self):
+        AdventDay.__init__(self, 18)
+
+    def answer_part1(self):
+        print("")
+
+    def answer_part2(self):
+        print("")
+
+    def iterate_lights(self, lights_in):
+        lights_out = [[0 for _ in range(len(lights_in[0]))] for _ in range(len(lights_in))]
+        for row in range(0, len(lights_in)):
+            for column in range(0, len(lights_in[0])):
+                neighbors_value = self.get_neighbors_value(lights_in, row, column)
+                if lights_in[row][column] and neighbors_value in [2, 3]:
+                    lights_out[row][column] = 1
+                elif not lights_in[row][column] and neighbors_value == 3:
+                    lights_out[row][column] = 1
+                else:
+                    lights_out[row][column] = 0
+
+
+    @staticmethod
+    def get_neighbors_value(lights, row, column):
+        neighbors_value = lights[row][column]
+        for vertical in range(-1, 2):
+            if (vertical + row) >= 0:
+                for horizontal in range(-1, 2):
+                    if (horizontal + column) >= 0:
+                        neighbors_value += lights[vertical][horizontal]
+        return neighbors_value
+
+    @staticmethod
+    def count_lights(lights):
+        return sum(sum(lights, []))
+
+    def get_initial_lights(self):
+        lights = []
+        for one_string in self.read_data():
+            column_list = []
+            for char in one_string:
+                if char is '#':
+                    column_list.append(1)
+                else:
+                    column_list.append(0)
+            lights.append(column_list)
+        return lights
+
+adventDay18_1 = AdventDayEighteen()
+adventDay18_2 = AdventDayEighteen()
+print("Day 18 Part 1: {answer}".format(answer=adventDay18_1.answer_part1()))
+print("Day 18 Part 2: {answer}".format(answer=adventDay18_2.answer_part2()))
